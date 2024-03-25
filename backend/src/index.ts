e951eb8b1d13653f7ec05b3e01af7985d714009f
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-03-25 10:43:24 +0530

    inititialize handlers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,25 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/signup", async (c) => {
+  return c.text("Hello, World!");
+});
 
-export default app
+app.post("/api/v1/signin", async (c) => {
+  return c.text("Hello, World!");
+});
+
+app.post("/api/v1/blog", async (c) => {
+  return c.text("Hello, World!");
+});
+
+app.put("/api/v1/blog", async (c) => {
+  return c.text("Hello, World!");
+});
+
+app.get("/api/v1/blog/:id", async (c) => {
+  return c.text("Hello, World!");
+});
+
+export default app;
