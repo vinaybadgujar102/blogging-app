a9ad5cd44c98449ab465f08e87a8e51dceda3934
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-03-25 16:35:07 +0530

    initialize prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const app = new Hono();
 
 app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+  }).$extends(withAccelerate());
   return c.text("Hello, World!");
 });
 
