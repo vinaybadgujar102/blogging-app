9a47050458f6b0bde488804f8027497f755ebf39
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-04-05 21:13:44 +0530

    complete authentication frontend

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,11 +1,27 @@
 import React, { ChangeEvent, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { BACKEND_URL } from "../config";
+import axios from "axios";
 
 const Auth = ({ type }: { type: "signup" | "signin" }) => {
+  const navigate = useNavigate();
   const [postInputs, setPostInputs] = useState({
     email: "",
     password: "",
   });
+
+  async function sendRequest() {
+    try {
+      const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type}`, {
+        ...postInputs,
+      });
+      const jwt = response.data;
+      localStorage.setItem("token", jwt);
+      navigate("/blogs");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div className="h-screen flex justify-center flex-col">
@@ -48,6 +64,7 @@ const Auth = ({ type }: { type: "signup" | "signin" }) => {
               }}
             />
             <button
+              onClick={sendRequest}
               type="button"
               className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
             >
