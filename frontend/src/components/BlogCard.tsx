ee8fe60df6c2844d70c7f86d18eefad69106ddcd
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-04-06 18:53:43 +0530

    add basic appbar

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -36,10 +36,24 @@ const BlogCard = ({
   );
 };
 
-function Avatar({ name }: { name: string }) {
+export function Avatar({
+  name,
+  size = "small",
+}: {
+  name: string;
+  size?: "small" | "big";
+}) {
   return (
-    <div className="relative inline-flex items-center justify-center w-6 h-6 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-      <span className="font-xs text-gray-600 dark:text-gray-300">
+    <div
+      className={`relative inline-flex items-center justify-center ${
+        size === "small" ? "w-6 h-6" : "w-10 h-10"
+      } overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}
+    >
+      <span
+        className={`${
+          size === "small" ? "text-xs" : "text-md"
+        } text-gray-600 dark:text-gray-300`}
+      >
         {name.charAt(0)}
       </span>
     </div>
