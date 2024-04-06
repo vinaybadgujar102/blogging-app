f28fb8013eb7193a1209ef09ad86dcb6bcd13aa0
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-04-06 17:58:23 +0530

    add more blogs

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -11,6 +11,18 @@ const Blogs = () => {
           authorName="Jayanth babu Sir"
           publishedDate="Jan 25, 2024"
         />
+        <BlogCard
+          title="4 useState Mistakes You Should Avoid in React🚫"
+          content="Introduction React.js has become a cornerstone of modern web development, with its unique approach to managing state within components. One common hook, useState, is fundamental but often"
+          authorName="Jayanth babu Sir"
+          publishedDate="Jan 25, 2024"
+        />
+        <BlogCard
+          title="4 useState Mistakes You Should Avoid in React🚫"
+          content="Introduction React.js has become a cornerstone of modern web development, with its unique approach to managing state within components. One common hook, useState, is fundamental but often"
+          authorName="Jayanth babu Sir"
+          publishedDate="Jan 25, 2024"
+        />
       </div>
     </div>
   );
