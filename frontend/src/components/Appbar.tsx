d5d3eb3bb2daf8363cd8448f208208ce72104848
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-04-07 19:51:59 +0530

    add publish page and route

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -9,6 +9,14 @@ const Appbar = () => {
         Blog
       </Link>
       <div>
+        <Link to={"/publish"}>
+          <button
+            type="button"
+            className="mr-8 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+          >
+            New
+          </button>
+        </Link>
         <Avatar name="Vinay" size={"big"} />
       </div>
     </div>
