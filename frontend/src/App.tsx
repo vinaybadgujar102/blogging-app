8c041f4b31b3064607c3a6ebfeef5a936a3e72e3
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-04-08 15:58:00 +0530

    when on index page navigate to signin route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Signin } from "./pages/Signin";
 import { Blog } from "./pages/Blog";
 
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Blogs from "./pages/Blogs";
 import Publish from "./pages/Publish";
 
@@ -11,6 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/blog/:id" element={<Blog />} />
